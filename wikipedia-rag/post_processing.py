12a25d96b28cfae998111f79b72b13b3c23e8fb7
Hyounsoo Kim <117959929+HyounsooKim@users.noreply.github.com>
2024-04-02 18:40:38 +0000

    Update default setting.

---
+++ wikipedia-rag/post_processing.py
@@ -0,0 +1,12 @@
+
+from promptflow import tool
+
+
+# The inputs section will change based on the arguments of the tool function, after you save the code
+# Adding type to arguments and return value will help the system show the types properly
+# Please update the function name/signature per need
+@tool
+def my_python_tool(search_result: dict):
+    # print(search_result[0]['original_entity']['text'])
+    # 검색된 여러개의 문서에서 original_entity의 text 부분만 배열로 만들어서 반환
+    return [entity['original_entity']['text'] for entity in search_result]
